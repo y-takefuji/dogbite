ff950f74bc5b917d94f7a50c4b92db56bd7c0b70
y-takefuji <159754486+y-takefuji@users.noreply.github.com>
2024-08-18 17:14:59 +0900

    Add files via upload

--- bites.py
+++ bites.py
@@ -6,7 +6,7 @@ df = pd.read_csv('DOHMH_Dog_Bite_Data.csv')
 # Count total bites per breed
 breed_bite_counts = df['Breed'].value_counts()
 
-# Show the top 10 breeds with the most bites
-top_10_breeds = breed_bite_counts.head(10)
-print(top_10_breeds)
+# Show the top 15 breeds with the most bites
+top_15_breeds = breed_bite_counts.head(15)
+print(top_15_breeds)
 
